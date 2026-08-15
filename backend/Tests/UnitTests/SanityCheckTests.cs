9af578493d2475a6389ff16c03901dbe8e78b2d3
LuisPeralta313 <elperaltamc@gmail.com>
2026-08-15 13:44:40 -0600

    ci(backend): habilitar ejecución de pruebas en el pipeline

---
+++ backend/Tests/UnitTests/SanityCheckTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Peredent.Api.Tests.UnitTests;
+
+public class SanityCheckTests
+{
+    [Fact]
+    public void Xunit_EstaCorriendo()
+    {
+        Assert.Equal(4, 2 + 2);
+    }
+}
