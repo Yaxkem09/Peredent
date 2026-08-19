c37ed6c0efb79713645c347de2eb7b171704711c
LuisPeralta313 <elperaltamc@gmail.com>
2026-08-18 21:51:13 -0600

    feat(historia-medica): agregar registro de historia médica en el alta de paciente (SCRUM-16)

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -3,3 +3,4 @@ export { authService } from './auth.service';
 export { pacientesService } from './pacientes.service';
 export { citasService } from './citas.service';
 export { inventarioService } from './inventario.service';
+export { historiaMedicaService } from './historia-medica.service';
